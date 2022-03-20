230742f944f77974a86b1e7b9817df30640f30b2
DESKTOP-7K3KU14\Ryan Beck <Ryanbeck010@gmail.com>
2022-03-20 13:02:21 +0000

    Site working

--- index.php
+++ index.php
@@ -1,42 +1,68 @@
 <?php
 include_once "header.php";
-include_once "include/totalget.inc.php";
 
 ?>
-<form>
-
-
-    <input type = "text"name="Thing" placeholder="Players 1 Char 1">
-    <button type='submit' value="submit" name="submit"> Submit</button>
-</form>
-<h2>Player 1 char 1</h2>
-
 <form method="POST">
-<input type = "text"name="Thing" placeholder="Players 1 Char 2">
-<button type='submit' value="submit" name="submit"> Submit</button>
-</form>
+<select name="to_user" class="form-control">
+<option value="pick">Choose a char</option>
 <?php
-include_once 'include/totalget.inc.php';
-    if (isset($_GET['submit']))
-    {
-        $result = $_GET['Thing' ];
-        RowToGet($result);
+include_once "include/dbh.inc.php";
+$sql = mysqli_query($conn, "SELECT Name From character_stats_sheet");
+$row = mysqli_num_rows($sql);
+while ($row = mysqli_fetch_array($sql)){
+echo "<option value='". $row['Name'] ."'>" .$row['Name'] ."</option>" ;
+}
+?>
+</select>
+<?php
+include_once "include/dbh.inc.php";
+$sql = mysqli_query($conn,"SELECT Name,Health,Defence,Magic_Defence,Speed,Movement FROM character_stats_sheet Where Name='to_user'");
+$row = mysqli_num_rows($sql);
+$names = $Health = $Defence = $Magic_Defence = $Speed = $Movement = array();
+if(isset($_POST['submit2']))
+{
+    $id = mysqli_real_escape_string($conn,$_POST['to_user']);
+    $runit="SELECT Name,Health,Defence,Magic_Defence,Speed,Movement FROM character_stats_sheet Where Name='$id'";
+    $result=mysqli_query($conn,$runit);
+    $names = $Health = $Defence = $Magic_Defence = $Speed = $Movement = array();
+    $resultCheck = mysqli_num_rows($result);
+    if ($resultCheck > 0){
+        while ($row = mysqli_fetch_assoc($result))
+        {
+           $names = $row['Name'];
+           $Health= $row['Health'];
+           $Defence= $row['Defence'];
+           $Magic_Defence= $row['Magic_Defence'];
+           $Speed= $row['Speed'];
+           $Movement= $row['Movement'];
+
+            echo "Name: " . $names. "<br>";
+            echo "Health: " . $Health. "<br>";
+            echo "Defence: " . $Defence. "<br>";
+            echo "Magic defence: ". $Magic_Defence. "<br>";
+            echo "Speed: " . $Speed. "<br>";
+            echo "Movement: " .  $Movement. "<br>";
+
+        }
     }
 
-?>
- <form>
-     <select id="Name">
-        <option value = "pick">Name</option>
-        <?php 
-        $sql=mysqli_query($conn,"SELECT Name FROM character_stats_sheet");
-        $row = mysqli_num_rows($sql);
-        while($row = mysqli_fetch_array($sql)){
-            echo "<option value='".$row['Name']."'>" .$row['Name']."</option>";
-        }
-        ?>
-     </select>
-     
-</form>
 
-</body>
-</html>
+}
+?>
+    <input type = "text"name="Power" placeholder="Power">
+    <input type = "text"name="Mod" placeholder="Modfier...1.25">
+    <button type='submit' value="submit2" name="submit2"> Submit</button>
+</form>
+<?php
+include_once "include/dbh.inc.php";
+if(isset($_POST['submit2']))
+{
+    $power = mysqli_real_escape_string($conn,$_POST['Power']);
+    $mod=mysqli_real_escape_string($conn,$_POST['Mod']);
+    echo "Power:".$power ."Mod:". $mod . "<br>Defence:".intval($Defence);
+    $onehundred = 100;
+    $damage = (intval($power)  * intval($mod)) * (intval($onehundred)/(intval($onehundred) + intval($Defence)));
+    echo "<br>".ceil($damage)." " .$damage;
+
+}
+
