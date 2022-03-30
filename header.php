a799cb8d627c9bfad3e0a30bb657488ab05aae0e
DESKTOP-7K3KU14\Ryan Beck <Ryanbeck010@gmail.com>
2022-03-30 18:18:18 +0100

    Testing oop

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
         <a href="index.php"></a>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="discover.php">About Us</a></li>
+            <li><a href="OOPTRY.php">Test</a></li>
             <li><a href="signup.php">Signup</a></li>
             <li><a href="login.php">Login</a></li>
 </nav>
